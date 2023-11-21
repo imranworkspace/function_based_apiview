b216d667be26fae7cf0df10cdd71dd46d88f65bf
imran-py <imran.dtc144@gmail.com>
2023-11-21 14:10:05 +0530

    first commit

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET','POST','PUT'])
+def chk_api(request):
+    if request.method=='GET':
+        return Response({'msg':'get request'})
+
+    if request.method=='POST':
+        print(request.data)
+        return Response({'msg':'post request'})
+
+    if request.method=='PUT':
+        print(request.data)
+    return Response({'msg':'put request'})
---
+++ myapp.py
@@ -0,0 +1,46 @@
+import requests
+import json
+
+_URL='http://127.0.0.1:8000/api/'
+
+def get_api(id=None):
+    data={}
+    if id is not None:
+        data={'id':id}
+    json_data=json.dumps(data)
+    r = requests.get(url=_URL,data=json_data)
+    data=r.json()
+    print('data',data)
+
+
+def post_api():
+    data={
+        "name":"imran",
+        "mobile":7710881086
+    }
+    # 
+    headers = {"content-Type":"application/json"}
+    json_data=json.dumps(data)
+    r = requests.post(url=_URL,headers=headers,data=json_data)
+    data=r.json()
+    print('data',data)
+
+def put_api(id=None):
+    if id is not None:
+        data={'id':id}
+    data={
+        "name":"imran",
+        "mobile":7710881086
+    }
+    # 
+    headers = {"content-Type":"application/json"}
+    json_data=json.dumps(data)
+    r = requests.put(url=_URL,headers=headers,data=json_data)
+    data=r.json()
+    print('data',data)
+
+
+# calling fun
+get_api()
+post_api()
+put_api()
